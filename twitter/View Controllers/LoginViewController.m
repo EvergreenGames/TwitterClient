2d9fdbc67a9e65b58061095bdcd4e341136d8c48
Ruben Green <ragreen@fb.com>
2020-07-02 16:17:53 -0700

    Fixed modal segue issue

--- twitter/View Controllers/LoginViewController.m
+++ twitter/View Controllers/LoginViewController.m
@@ -8,6 +8,7 @@
 
 #import "LoginViewController.h"
 #import "APIManager.h"
+#import "AppDelegate.h"
 
 @interface LoginViewController ()
 
@@ -28,7 +29,11 @@
 - (IBAction)didTapLogin:(id)sender {
     [[APIManager shared] loginWithCompletion:^(BOOL success, NSError *error) {
         if (success) {
-            [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+            AppDelegate* appDelegate = (AppDelegate*)[UIApplication sharedApplication].delegate;
+            
+            UIStoryboard* storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+            LoginViewController* loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"TweetsNavigationController"];
+            appDelegate.window.rootViewController = loginViewController;
         } else {
             NSLog(@"%@", error.localizedDescription);
         }
